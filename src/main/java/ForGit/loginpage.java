3c7de0787408593590fbce4d9e5c0f12821e12bd
Adhil-Subair <adhilsubair1@gmail.com>
2024-09-19 13:29:57 +0530

    commits completed

--- src/main/java/ForGit/loginpage.java
+++ src/main/java/ForGit/loginpage.java
@@ -11,7 +11,12 @@ public class loginpage {
 		System.out.println("added from local");
 
 		System.out.println("second addition");
+
 		System.out.println("next addition");
+
+		
+		System.out.println("final");
+
 
 
 	}
