39c963d534eed79d464f9e360a53d0de72819b34
Adhil-Subair <adhilsubair1@gmail.com>
2024-09-19 13:06:17 +0530

    commited latest

--- src/main/java/ForGit/loginpage.java
+++ src/main/java/ForGit/loginpage.java
@@ -5,7 +5,11 @@ public class loginpage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Logged in successfully");
+
 		System.out.println("Added from remote");
+
+		System.out.println("added from local");
+
 
 	}
 
